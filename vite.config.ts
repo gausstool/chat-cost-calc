a57e82a091e1b7c911f90b6de483be8386f4c14b
gausszhou <gausszhou@qq.com>
2026-04-26 23:59:35 +0800

    refactor: update build script and add gh-pages deployment script

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from 'node:path';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import 'dotenv/config';
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(async () => ({
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [vue()],
-})
+  clearScreen: false,
+  server: {
+    port: 1998,
+    strictPort: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve('src'),
+    },
+  },
+  build: {
+    outDir: process.env.VITE_BUILD_DIR || 'dist',
+  },
+}));
